801ce99d18276abb9e7483604a4fb79b4183278f
Marco Aurelio Barriga <marcoaureliobarriga67@gmail.com>
2025-06-11 16:44:49 -0400

    Delete Lista_grupo.py

--- Lista_grupo.py
+++
@@ -1,5 +0,0 @@
-#Lista de programación II
-#Death Services
-Death_services = ["Adaniel Balderrama Orellana", "Marco Aurelio Barriga", "Carlos Ademar Suarez"]
-for nombre in Death_services:
-    print(f"¡Bienvenido al grupo {nombre}!")
