1422b652576da2671d8b7d5ad4464cd6cad548bb
Marco Aurelio Barriga <marcoaureliobarriga67@gmail.com>
2025-06-11 09:06:11 -0400

    Add files via upload
    Lista de integrantes

---
+++ Lista_grupo.py
@@ -0,0 +1,5 @@
+#Lista de programación II
+#Death Services
+Death_services = ["Adaniel Balderrama Orellana", "Marco Aurelio Barriga", "Carlos Ademar Suarez"]
+for nombre in Death_services:
+    print(f"¡Bienvenido al grupo {nombre}!")
