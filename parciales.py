5f5e51ffb4d0f493daaa145221de38e26ed1d624
Marco Aurelio Barriga <marcoaureliobarriga67@gmail.com>
2025-06-11 17:55:10 -0400

    Add files via upload

---
+++ parciales.py
@@ -0,0 +1,10 @@
+notas_parciales = [80, 95, 73, 60, 88]
+primera_nota = notas_parciales[0] 
+print(f"La primera nota fue: {primera_nota}") 
+tercera_nota = notas_parciales[2] 
+print(f"La tercera nota fue: {tercera_nota}")
+print(f"Lista original: {notas_parciales}")
+notas_parciales[3] = 65 
+print(f"Lista modificada: {notas_parciales}")
+cantidad_de_notas = len(notas_parciales)
+print(f"Tenemos un total de {cantidad_de_notas} notas.") 
