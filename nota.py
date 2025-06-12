12fa8ff8ec4d160c24bccb4b3e36e3685c9c739b
Marco Aurelio Barriga <marcoaureliobarriga67@gmail.com>
2025-06-12 07:43:23 -0400

    Add files via upload

---
+++ nota.py
@@ -0,0 +1,8 @@
+notas = [85.5, 92, 78, 88.5, 95, 82]
+suma_t = 0
+for nota in notas:
+    suma_t += nota
+promedio = suma_t / len(notas)
+print(f"Suma total de las notas: {suma_t}")
+print(f"Promedio de las notas: { promedio:.2f} ")
+print(f"¡No`mbre a veces soy una cosa pero bárbara!")
