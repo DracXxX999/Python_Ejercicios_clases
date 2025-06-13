d2d55efd86856e1ad17e1e16815f0b833012a56a
Marco Aurelio Barriga <marcoaureliobarriga67@gmail.com>
2025-06-13 08:12:03 -0400

    Delete hobbies.py

--- hobbies.py
+++
@@ -1,8 +0,0 @@
-#Lista de hobbies
-hobbies = ["Dibujar, Jugar codsito, comer"]
-lista_hobbies = hobbies
-print (f"Estos son mis hobbies: {lista_hobbies}")
-#Lista de comidas favoritas
-comidas = ["Picante de pollo, Pomllito frito, Sopita de mani"]
-lista_comida = comidas
-print (f"Estos son mis comidas favoritas: {lista_comida}")
