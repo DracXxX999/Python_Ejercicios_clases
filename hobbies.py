bd904890d2e00b67f6b74e8dcde3fa9bd40c6c90
Marco Aurelio Barriga <marcoaureliobarriga67@gmail.com>
2025-06-13 08:12:33 -0400

    Add files via upload

---
+++ hobbies.py
@@ -0,0 +1,8 @@
+#Hobbies 
+hobbies_M = ["Dibujar", "Jugar codsito", "Realizar Isp pinout"]
+lista_hobbies =  hobbies_M
+print(f"Estos son mis hobbies: {lista_hobbies}")
+print(f"Mi segundo hobbies es: {lista_hobbies[1]}")
+comidas_favoritas = ["Picante de pollo", "Pomllito frito", "Sopa de mani" ]
+print(f"Estas son mis comidas favoritas: {comidas_favoritas}")
+print(f"Mi segunda comida: {comidas_favoritas[1]}")
