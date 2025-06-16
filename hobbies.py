271b9ab9e2795a7876f2b8fe6b41f70696d4a694
Jim Nataniel <95052718+bolivianotech@users.noreply.github.com>
2025-06-16 09:18:45 -0400

    Update with teache to see index access hobbies.py

--- hobbies.py
+++ hobbies.py
@@ -5,4 +5,7 @@ print(f"Estos son mis hobbies: {lista_hobbies}")
 print(f"Mi segundo hobbies es: {lista_hobbies[1]}")
 comidas_favoritas = ["Picante de pollo", "Pomllito frito", "Sopa de mani" ]
 print(f"Estas son mis comidas favoritas: {comidas_favoritas}")
-print(f"Mi segunda comida: {comidas_favoritas[1]}")
+print(f"Mi segunda comida cambiara de: {comidas_favoritas[1]}")
+comidas_favoritas[1] = "Salteña"
+print(f"Mi segunda comida cambiara a: {comidas_favoritas[1]}")
+print(f"Ahora estas son mis comidas favoritas: {comidas_favoritas}")
