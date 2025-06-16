e2efd67f5153be4b7d962252d3a1af5880d21444
Marco Aurelio Barriga <marcoaureliobarriga67@gmail.com>
2025-06-16 17:52:54 -0400

    Delete elementos.py

--- elementos.py
+++
@@ -1,7 +0,0 @@
-def acumulador_suma(lista_numeros):
-    lista_numeros = [1, 2, 3, 4, 5]
-    acumulador_suma = 0
-    for elemento_actual in lista_numeros: 
-        acumulador_suma += elemento_actual
-    return acumulador_suma
-print(acumulador_suma([1, 2, 3, 4, 5]))
